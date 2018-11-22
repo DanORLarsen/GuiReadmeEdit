5cb7b4f3cd3e8fb96dc58032caf953cc344541e8
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-11-22 11:08:58 +0100

    Det virkede tilsidst, Den sletter alt hvad der stod før, og skriver noget nyt.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import javafx.scene.control.TextArea;
+
+
+
+
 public class Controller {
+    @FXML
+    TextArea guiSkriver;
+    public void writeReadme(ActionEvent actionEvent) throws FileNotFoundException {
+        File readme = new File("README.md");
+        System.out.println("Filen findes? = " + readme.exists());
+        PrintWriter fileSkriver = new PrintWriter(readme);
+        try {
+            fileSkriver.print("#README FILEN\n");
+            Scanner sc = new Scanner(readme);
+                fileSkriver.println(guiSkriver.getText());
+        }
+        catch (Exception e)
+        {
+            System.out.println("Noget gik galt");
+        }
+        fileSkriver.close();
+
+
+    }
 }
